c616633a9a54781bd7abf16c371f17ee933f358e
Andy Tran <transient.tran@gmail.com>
2016-12-07 01:18:19 -0500

    solved day 3 part 2 with more math and some help from reddit

--- day3p2.js
+++ day3p2.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const readStream = fs.createReadStream('day3input.txt');
 
 let data;
+let convertedData;
 let counter = 0;
 
 readStream.on('data', (chunk) => {
@@ -9,53 +10,68 @@ readStream.on('data', (chunk) => {
 });
 
 readStream.on('end', () => {
-  processData(data);
+  processData();
 });
 
-const processData = (data) => {
-  data.forEach((datum, i) => {
-    const triangle = cleanUp(datum);
-    calculateSums(triangle);
+const processData = () => {
+  let tri = [];
+  var cleanData = data.map((d) => {
+    return d.trim();
+  })
+
+  cleanData.forEach((datum, i) => {
+    if (tri.length % 3 != 0 && tri.length < 3 || tri.length < 3) {
+      tri.push(datum.trim().split('  '));
+    }
+
+    if (tri.length == 3) {
+      processMatrix(tri);
+      tri = [];
+    }
 
     if (i == data.length - 1) {
-      console.log(data.length - counter); // in calculateSums we check for how many are impossible triangles. subtract that from the total number of triangles
+      console.log(counter); // in calculateSums we check for how many are impossible triangles. subtract that from the total number of triangles
     }
   })
 };
 
-const cleanUp = (datum) => {
-  if (datum) {
-    return datum.split(' ').filter((el) => {
-      return el != ''
-    })      
+// example:
+// [ [ '810', '679', ' 10' ],
+//   [ '783', '255', '616' ],
+//   [ '545', '626', '626' ] ]
+
+const processMatrix = (data) => {
+  var trinity = data.map(function(d) {
+    return cleanUp(d)
+  })
+
+  const tri = trinity;
+
+  for (let i = 0; i < tri.length; i++) {
+    let triangle = [tri[0][i], tri[1][i], tri[2][i]]
+    calculateSums(triangle);
   }
+
+}
+
+const cleanUp = (datum) => {
+  const data = datum.map(function(d) {
+    return d.trim();
+  })
+
+  return data.filter((el) => {
+    return el != ''
+  })      
 };
 
-const calculateSums = (triangle) => {
-  var pass = false; // to check if we should keep going in the current calculations
-  var one, two, three; // these will hold the removed elements
-  var triangleOne = triangle.slice(0,3), triangleTwo = triangle.slice(0,3), triangleThree = triangle.slice(0,3); // these will be manipulated
+const calculateSums = (data) => {
+  const triangle = data.map(function(d) {
+    return parseInt(d);
+  })
 
-  one = parseInt(triangleOne.shift());
-  two = parseInt(triangleTwo.pop());
-  three = parseInt(triangleThree.splice(1, 1));
-
-  const comparisons = [one, two, three];
-  const calculations = [triangleOne, triangleTwo, triangleThree];
-
-  calculations.forEach((numbers, i) => {
-    const compareWith = comparisons[i];
-    let sum = 0;
-
-    if (pass == false) {
-      sum = numbers.reduce(function(a,b) {
-        return parseInt(a) + parseInt(b)
-      })  
-
-      if (sum <= compareWith) {
-        counter += 1;
-        pass = true;
-      }
-    };
-  });
+  let sum = triangle[0] + triangle[1] + triangle[2];
+  let max = Math.max(Math.max(triangle[0], triangle[1]), triangle[2]);
+  if (sum - max > max) {
+      counter++;
+  }
 }
